46b164b8e19dad10946e996d7c5914ed33d09cee
otsuka <kaho-otsuka@kyms.co.jp>
2018-04-26 17:49:04 +0900

    コミットテスト

---
+++ src/util/CommonUtil.java
@@ -0,0 +1,22 @@
+package util;
+
+/**
+ * 共通Util
+ *
+ * @author otsuka-kaho
+ *
+ */
+public class CommonUtil {
+	// DB関連
+	// Oracle
+	public String db_oracle = "oracle.jdbc.driver.OracleDriver";
+	// MySQL
+	public String db_mysql = "com.mysql.jdbc.Driver";
+	// URL(mysql)
+	public String db_url = "jdbc:mysql://192.168.212.104/chat_test?characterEncoding=UTF8";
+	// ユーザー
+	public String db_user = "kmsuser";
+	// パスワード
+	public String db_password = "userkms";
+
+}
